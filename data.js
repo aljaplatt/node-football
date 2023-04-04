1db44aca7a3ff5094b83213f054b3ce95d83529f
Alex <alja.platt@gmail.com>
2023-04-04 09:55:44 +0100

    get team by id

---
+++ data.js
@@ -0,0 +1,19 @@
+// const batistuta = {
+//     "firstName": "Gabriel",
+//     "surname": "Batistuta",
+//     "position": "Striker",
+//     "team": "Fiorentina",
+//     "nationality": "Argentina",
+//     "rating": 95
+// }
+
+const teams = [{
+    teamName: 'Manchester United',
+    League: 'Premier League',
+    Nation: 'England,',
+    manager: 'Erik Ten Hag',
+    numLeagueTitles: 20,
+    established: 1878
+}]
+
+export default teams
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid'
+
+import teams from "./data.js";
 
 // initialise express - call function and 
 const app = express();
@@ -7,28 +10,31 @@ const PORT = 3000;
 // parse request body as JSON
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    const batistuta = {
-        "firstName": "Gabriel",
-        "surname": "Batistuta",
-        "position": "Striker",
-        "team": "Fiorentina",
-        "nationality": "Argentina",
-        "rating": 95
-    }
+app.get("/teams", (req, res) => {
     res.json({
         "key" : "value",
-        "payload": batistuta
+        "payload": teams
+    })
+})
+app.get("/team/:teamId", (req, res) => {
+    console.log(req.params)
+    const teamId = req.params.teamId
+    const requestedTeam = teams.find(obj => obj.id === teamId)
+    // console.(teamId)
+    res.json({
+        "key" : "value",
+        "payload": requestedTeam
     })
 })
 
-app.post("/add-player", (req, res) => {
+app.post("/add-team", (req, res) => {
     const request = req.body
+    request.id = uuidv4()
     console.log(request)
-
+    teams.push(request)
     res.json({
         "message": "success",
-        "payload": request
+        "payload": teams
     })
 })
 
